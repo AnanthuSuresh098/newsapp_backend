b94e6dbfdb77b7c465d9e2cea4fa6a2b1d0f1408
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-06-15 17:03:10 +0530

    Topstories controller commit

--- src/controllers/MyTopStoriesController.js
+++ src/controllers/MyTopStoriesController.js
@@ -1,17 +1,29 @@
 const express = require("express");
 
+const axios = require("axios");
+
 const router = express.Router();
 
 router.get("", async (req, res) => {
   try {
-    return res.send();
+    let response = await axios.get(
+      `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
+    );
+
+    const TopstoriesList = [];
+
+    const topstoryMap = response.data.map(async (id) => {
+      let getstory = await axios.get(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+      );
+      TopstoriesList.push(getstory.data);
+    });
+
+    await Promise.all(topstoryMap);
+
+    return res.status(200).send({ TopStories: TopstoriesList });
   } catch (err) {
     return res.status(500).send(err);
   }
 });
-
-router.post("", async (req, res) => {
-  return res.send();
-});
-
 module.exports = router;
