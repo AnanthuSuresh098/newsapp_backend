0ebe4552ed849926f942b05b8bc437f368315af7
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-06-16 13:07:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,13 +13,9 @@ const app = express();
 app.use(express.json());
 
 
-app.use((req, res, next) => {
-   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+app.get("/cors", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.send({ msg: "This has CORS enabled " });
 });
 
 app.use("/topstories", MyTopStoriesController);
